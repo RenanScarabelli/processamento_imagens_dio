4c46779157127ffdd1ea02052d2460cbc72ecb1d
RenanScarabelli <renanscarabelli@gmail.com>
2022-10-26 23:14:40 -0300

    Edição do arquivo de Setup e do readme.MD

--- setup.py
+++ setup.py
@@ -7,14 +7,14 @@ with open("requirements.txt") as f:
     requirements = f.read().splitlines()
 
 setup(
-    name="package_name",
+    name="image_processing",
     version="0.0.1",
-    author="my_name",
-    author_email="my_email",
-    description="My short description",
+    author="Renan_Scarabelli",
+    author_email="renanscarabelli@gmail.com",
+    description="Pacote criado para entrega de projeto na DIO",
     long_description=page_description,
     long_description_content_type="text/markdown",
-    url="my_github_repository_project_link"
+    url="https://github.com/RenanScarabelli/processamento_imagens_dio"
     packages=find_packages(),
     install_requires=requirements,
     python_requires='>=3.8',
